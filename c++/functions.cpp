5d445399222b53c10b0abdc4592e381018ca4467
Gabe41 <p_gabriel@ymail.com>
2023-10-29 15:08:41 -0400

    some more basic functions added

--- c++/functions.cpp
+++ c++/functions.cpp
@@ -2,6 +2,33 @@
 #include <cmath>
 #include <vector>
 
+bool is_palindrome(std::string text) {
+    int left = 0;
+    int right = text.length() - 1;
+
+    while (left < right) {
+        if (text[left] != text[right]) {
+            return false; // If characters don't match, it's not a palindrome
+        }
+        left++;
+        right--;
+    }
+
+    return true; // If the loop completes without finding non-matching characters, it's a palindrome
+}
+
+std::string needs_water(int days, bool is_succulent) {
+  if (!is_succulent && days > 3) {
+        return "Time to water the plant.";
+  } else if (is_succulent && days <= 12) {
+        return "Don't water the plant!";
+  } else if (is_succulent && days >= 13) {
+        return "Go ahead and give the plant a little water.";
+  } else {
+        return "Don't water the plant!";
+  }
+}
+
 std::vector<int> first_three_multiples(int num) {
   std::vector<int> result = {1, 2, 3};
   for (int i = 0; i < result.size(); ++i) {
@@ -22,6 +49,16 @@ int tenth_power(int num) {
 
 int main() {
   
+  std::cout << is_palindrome("madam") << "\n";
+  std::cout << is_palindrome("ada") << "\n";
+  std::cout << is_palindrome("lovelace") << "\n";
+  
+  std::cout << needs_water(10, false) << "\n";
+
+  for (int element : first_three_multiples(8)) {
+    std::cout << element << "\n";
+  }
+  
   std::cout << average(42.0, 24.0) << "\n";
   std::cout << average(1.0, 2.0) << "\n";
 
@@ -29,8 +66,8 @@ int main() {
   std::cout << tenth_power(1) << "\n";
   std::cout << tenth_power(2) << "\n";
 
-  for (int element : first_three_multiples(8)) {
-    std::cout << element << "\n";
-  }
+  
+
+  
   
 }
